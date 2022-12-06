4d011f7d2afac640320a5eaaa2d2557667468c5f
ryanvle <ryanvle@uw.edu>
2022-12-05 18:44:27 -0800

    updated photo

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -7,7 +7,7 @@ export default [
     },
     {
         title: 'Developer Portfolio',
-        imgUrl:  '',
+        imgUrl:  'assests/portfolio.png',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://hungryhuskies-de409.web.app/',
      },
