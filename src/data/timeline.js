ffdbe956c872e961c298fd0a3f5366f0d73b768d
ryanvle <ryanvle@uw.edu>
2022-12-07 12:07:11 -0800

    updated timeline descriptions

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,18 +3,18 @@ export default [
         year: 'Today',
         title: 'University of Washington',
         duration: '3 years',
-        details: 'University is cool'
+        details: "Direct admitted into the Informatics program at the University of Washington. Currently just journaling and taking in all the information/experience while I'm here. Elected as President of Husky Wushu and became a performance member for Taiko Kai."
     },
     {
         year: '2021',
-        title: 'Finished Highline College',
+        title: 'Completed Highline College',
         duration: '2 years',
-        details: 'Running Start'
+        details: 'Enrolled in the Running Start program. Took classes at Highline College for my last 2 years of high school. Shaped me into a lethal academic weapon so that I can pursue higher education.'
     },
     {
         year: '2021',
         title: 'High School Diploma',
         duration: '4 years',
-        details: 'Dirty Federal Way'
+        details: 'Graduated from Federal Way High School. Activities that I did were Anime Club, Tennis and Robotics. Supervised Anime Club as the president, raised over $500 through fundraising to help students go to Sakura-Con. For Tennis, I coached 2 freshman my junior year that enabled them to make it to state their senior year. Lead Programmer for Robotics, programmed and engineered the drive train for the 2019 competition bot that went on to win a district championship.'
     }
 ]
